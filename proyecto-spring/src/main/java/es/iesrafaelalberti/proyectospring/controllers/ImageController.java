d1846b29b34fce0caec14a4b9cb1a42d181b981b
Carolina <crodriguez@iesrafaelalberti.es>
2023-06-15 03:09:40 +0200

    CONTROLLER IMAGE

--- proyecto-spring/src/main/java/es/iesrafaelalberti/proyectospring/controllers/ImageController.java
+++ proyecto-spring/src/main/java/es/iesrafaelalberti/proyectospring/controllers/ImageController.java
@@ -1,7 +1,29 @@
 package es.iesrafaelalberti.proyectospring.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import es.iesrafaelalberti.proyectospring.models.Image;
+import es.iesrafaelalberti.proyectospring.services.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Base64;
 
 @RestController
 public class ImageController {
+    @Autowired
+    ImageService imageService;
+
+    @PostMapping("/images/add")
+    public String addPhoto(@RequestParam("title") String title,
+                           @RequestParam("image") MultipartFile image)
+            throws IOException {
+        return imageService.addImage(title, image);
+    }
+
+    @GetMapping("/images/{id}")
+    public String getPhoto(@PathVariable String id) {
+        Image image = imageService.getImage(id);
+        return "<img src=\"data:image/jpeg;base64, " + Base64.getEncoder().encodeToString(image.getImage().getData()) + "\">";
+    }
 }
